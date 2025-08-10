7054d3e365e666192449af44f0b2fc6027969664
Bowen Chen <darbra@126.com>
2025-08-10 20:25:39 +0800

    chore: 更新导航数据 - 2025/8/10 20:25:39

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -1,39 +1,10 @@
 export const mockData = {
   "categories": [
     {
-      "id": "community",
-      "name": "社区论坛",
-      "icon": "👥",
-      "order": 2,
-      "sites": [
-        {
-          "id": "linuxdo",
-          "name": "Linux.do",
-          "url": "https://linux.do",
-          "description": "Linux与开源技术社区",
-          "icon": "/sitelogo/linux.do.ico"
-        },
-        {
-          "id": "v2ex",
-          "name": "V2EX",
-          "url": "https://www.v2ex.com",
-          "description": "创意工作者社区",
-          "icon": "/sitelogo/www.v2ex.com.ico"
-        },
-        {
-          "id": "52pojie",
-          "name": "吾爱破解",
-          "url": "https://www.52pojie.cn/",
-          "description": "软件安全与破解技术论坛",
-          "icon": "/sitelogo/www.52pojie.cn.ico"
-        }
-      ]
-    },
-    {
       "id": "category-1754634607371",
       "icon": "📚",
       "name": "资源",
-      "order": 1,
+      "order": 0,
       "sites": [
         {
           "id": "site-1754634637402",
@@ -62,6 +33,35 @@ export const mockData = {
           "url": "http://47.123.4.186:33333/",
           "description": "看电影",
           "icon": ""
+        }
+      ]
+    },
+    {
+      "id": "community",
+      "name": "社区论坛",
+      "icon": "👥",
+      "order": 1,
+      "sites": [
+        {
+          "id": "linuxdo",
+          "name": "Linux.do",
+          "url": "https://linux.do",
+          "description": "Linux与开源技术社区",
+          "icon": "/sitelogo/linux.do.ico"
+        },
+        {
+          "id": "v2ex",
+          "name": "V2EX",
+          "url": "https://www.v2ex.com",
+          "description": "创意工作者社区",
+          "icon": "/sitelogo/www.v2ex.com.ico"
+        },
+        {
+          "id": "52pojie",
+          "name": "吾爱破解",
+          "url": "https://www.52pojie.cn/",
+          "description": "软件安全与破解技术论坛",
+          "icon": "/sitelogo/www.52pojie.cn.ico"
         }
       ]
     }
